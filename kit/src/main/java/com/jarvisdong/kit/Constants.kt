a1b7100b645b12b3fee9c5cfd65b6dc8b448022f
JarvisDong <dongmm@handsure.com>
2018-12-10 09:01:52 +0800

    all

--- kit/src/main/java/com/jarvisdong/kit/Constants.kt
+++ kit/src/main/java/com/jarvisdong/kit/Constants.kt
@@ -3,6 +3,11 @@ package com.jarvisdong.kit
 /**
  * Created by JarvisDong on 2018/12/6.
  * OverView:
+ * 常量有三种类型
+ * 1. 直接const定义常量,使用 文件名.常量名
+ * 2. 对象中使用const定义常量 对象名.常量名
+ * 3. class中使用const定义常量 类名.常量名
+ * 4. @jvmField注解,定义java中静态常量
  */
 
 //activity类型
@@ -12,8 +17,14 @@ object ActBottomType{
     const val ACT_BOTTOM_SHARE = 2
 }
 
-//class Constants{
-//    companion object {
-//        const val CONST2 = 1;
-//    }
-//}
+//六大原则
+class Principle{
+    companion object {
+        const val SRP = 0  //
+        const val OCP = 1
+        const val LSP = 2
+        const val DIP = 3
+        const val ISP = 4
+        const val LKP = 5
+    }
+}
