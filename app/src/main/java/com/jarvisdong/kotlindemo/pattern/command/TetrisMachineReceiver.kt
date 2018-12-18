9606a70a6b6ac8e9d14534bbcc4acdac496155d6
mac <1961473915@qq.com>
2018-12-18 10:53:25 +0800

    all

---
+++ app/src/main/java/com/jarvisdong/kotlindemo/pattern/command/TetrisMachineReceiver.kt
@@ -0,0 +1,28 @@
+package com.jarvisdong.kotlindemo.pattern.command
+
+/**
+ * Created by JarvisDong on 2018/12/17.
+ * OverView:
+ *
+ * 设计模式10--命令模式
+ *
+ * 命令模式--接受者模型(真正处理逻辑)
+ */
+class TetrisMachineReceiver {
+
+    fun toLeft(){
+
+    }
+
+    fun toRight(){
+
+    }
+
+    fun fastToBottom(){
+
+    }
+
+    fun transformToTop(){
+
+    }
+}
