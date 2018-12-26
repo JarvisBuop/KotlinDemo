3518e4cccf0c82cb95099a02e3af650c5c08f861
mac <1961473915@qq.com>
2018-12-26 18:43:05 +0800

    all

---
+++ app/src/main/java/com/jarvisdong/kotlindemo/pattern/visitor/EngineerElement.kt
@@ -0,0 +1,20 @@
+package com.jarvisdong.kotlindemo.pattern.visitor
+
+import java.util.*
+
+/**
+ * Created by JarvisDong on 2018/12/26.
+ * OverView:
+ *
+ * [具体元素]
+ */
+class EngineerElement public constructor(aName :String): Element(aName){
+
+    fun getCodeLines() : Int{
+        return Random().nextInt(10*10000)
+    }
+
+    override fun accept(visitor: Visitor) {
+        visitor.visitElementA(this)
+    }
+}
---
+++ app/src/test/java/com/jarvisdong/kotlindemo/pattern/template/TestTemplate.kt
@@ -0,0 +1,20 @@
+package com.jarvisdong.kotlindemo.pattern.template
+
+import org.junit.Test
+
+/**
+ * Created by JarvisDong on 2018/12/26.
+ * OverView:
+ *
+ * 测试模板模式;
+ */
+class TestTemplate {
+    @Test
+    fun testTemplate() {
+        var template = NormalComputerTemplate()
+        template.startUp()
+
+        var template2 = MilitaryComputerTemplate()
+        template2.startUp()
+    }
+}
