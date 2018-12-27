c7785130dd6650fe2f0bbe9109f0cb2d554f18c5
mac <1961473915@qq.com>
2018-12-27 12:00:49 +0800

    all

---
+++ app/src/test/java/com/jarvisdong/kotlindemo/pattern/facade/TestFacade.kt
@@ -0,0 +1,19 @@
+package com.jarvisdong.kotlindemo.pattern.facade
+
+import org.junit.Test
+
+/**
+ * Created by JarvisDong on 2018/12/27.
+ * OverView:
+ *
+ * 测试外观模式
+ */
+class TestFacade{
+
+    @Test
+    fun testFacade(){
+        var mobile = MobileFacade()
+
+        mobile.videoChat()
+    }
+}
