ed64a38eeff7d64879007d663171f0f67cdb9329
dongmm <mm.dong@trip.com>
2020-12-10 18:59:23 +0800

    kotlin coroutine test

--- app/src/test/java/com/jarvisdong/kotlindemo/mock/TestMock.kt
+++ app/src/test/java/com/jarvisdong/kotlindemo/mock/TestMock.kt
@@ -7,10 +7,22 @@ import org.junit.Test
  * OverView:
  */
 
+class TestInterface{
+    fun a(){
+        System.out.println("test a")
+    }
+}
+
 class TestMock{
+
+    fun testImplFunc(call: TestInterface.()->Unit){
+        call.invoke(TestInterface())
+    }
 
     @Test
     fun testMockAnno() {
-
+        testImplFunc {
+            System.out.println("invoke $this ${this.a()}")
+        }
     }
 }
